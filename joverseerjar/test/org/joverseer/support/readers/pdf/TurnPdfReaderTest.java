0313e0aa05b333f3ec09e7c4abdce4d0458178c8
Dave Spring <github@davespring.org.uk>
2016-10-20 11:35:34 +0100

    extract text from PDF even if new xml, so users can see it in the pdfview. unit testing added for txt2xml just a simple confidence test to show parsing pdf to text and to xml. Still needs tweaking to make the locations of junit more generic.

---
+++ joverseerjar/test/org/joverseer/support/readers/pdf/TurnPdfReaderTest.java
@@ -0,0 +1,40 @@
+package org.joverseer.support.readers.pdf;
+
+import junit.framework.TestCase;
+import java.io.File;
+
+public class TurnPdfReaderTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testReadFile() {
+		TurnPdfReader r = new TurnPdfReader(null, "C:\\Users\\Dave\\Documents\\GitHub\\joverseer\\joverseerjar\\test\\org\\joverseer\\support\\readers\\pdf\\test1.pdf");
+		r.pdfTextFile = new File(r.filename + ".txt");
+		try {
+			r.parsePdf();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("exception");
+		}
+		assertTrue("no output file",r.pdfTextFile.exists());
+		r = new TurnPdfReader(null, "C:\\Users\\Dave\\Documents\\GitHub\\joverseer\\joverseerjar\\test\\org\\joverseer\\support\\readers\\pdf\\test2.pdf");
+		r.pdfTextFile = new File(r.filename + ".txt");
+		try {
+			r.parsePdf();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("exception");
+		}
+		assertTrue("no output file",r.pdfTextFile.exists());
+		
+	}
+
+}
---
+++ txt2xmljar/test/org/txt2xml/cli/BatchTest.java
@@ -0,0 +1,67 @@
+package org.txt2xml.cli;
+
+import static org.junit.Assert.*;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BatchTest {
+
+	final String OUTPUT_FILE =  "src/org/txt2xml/cli/sample.txt.xml";
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		Files.deleteIfExists(Paths.get(OUTPUT_FILE));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		final String EOL="\r\n";
+		try {
+			Batch.main(new String[]{"src/org/txt2xml/cli/sample.xml","src/org/txt2xml/cli/sample.txt"});
+		byte[] encoded = Files.readAllBytes(Paths.get(OUTPUT_FILE));
+		assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><txt2xml>"+EOL+
+					"<line>"+EOL+
+					"<field>1</field>"+EOL+
+					"<field>2</field>"+EOL+
+					"<field>3</field>"+EOL+
+					"<field>4&#13;</field>"+EOL+
+					"</line>"+EOL+
+					"<line>"+EOL+
+					"<field>a</field>"+EOL+
+					"<field>b</field>"+EOL+
+					"<field>c&#13;</field>"+EOL+
+					"</line>"+EOL+
+					"<line>"+EOL+
+					"<field>x</field>"+EOL+
+					"<field>y</field>"+EOL+
+					"<field>z</field>"+EOL+
+					"</line>"+EOL+
+					"</txt2xml>" +EOL
+					,new String(encoded, StandardCharsets.UTF_8));
+
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		fail("exception");
+	}
+	}
+}

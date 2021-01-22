2606611a3b7f05f2da24454f5c26f724336e5ec0
Dave Spring <github@davespring.org.uk>
2021-01-22 15:24:08 +0000

    no3D options
    Added a batch file to start JO with all the 3D drawing options turned off, for when testing suspected graphics drawing bugs.

---
+++ jOverseerRelease/src/joverseer-no3D.bat
@@ -0,0 +1,2 @@
+cd /d %~dp0
+java -Xmx512M -jar joverseer.jar -Djava.net.preferIPv4Stack=true java -Xmx512M -jar joverseer.jar -Djava.net.preferIPv4Stack=true -Dsun.java2d.ddoffscreen=false -Dsun.java2d.d3d=false -Dsun.java2d.noddraw=true %1

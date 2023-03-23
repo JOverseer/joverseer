9316fde507b3574894ec087d4619b1b24bd4909b
Dave Spring <>
2023-03-23 22:28:00 +0000

    bat file tweaking

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 setlocal
+rem for running on Dave's machine.
 set ANT=c:\lib\apache-ant-1.9.6\bin\ant.bat
-set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_172
-%ANT% test
-rem %ANT% -f joverseerRelease\build.xml
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-11.0.16.101-hotspot
+set JDK11_DAVE_DIR=%JAVA_HOME%
+rem %ANT% test
+
+%ANT% -f joverseerRelease\build.xml windowsInstaller
 endlocal

0f04e5f2c6678eb0017ea0d673f3ceba998beb9e
Dave Spring <>
2023-07-24 15:21:58 +0100

    Fix the JOverseer.bat file.
    Well that was a mess.
    See the usage in the comments of the joverseer.bat file.
    It makes joverseer-no3D.bat obsolete  - instead, run
    joverseer.bat DontUse3d
    It also makes joverseer-BYO-java.bat obsolete.

--- jOverseerRelease/src/joverseer.bat
+++ jOverseerRelease/src/joverseer.bat
@@ -1,7 +1,20 @@
 @echo off
+rem A batch file to invoke java with the right options to start JOverseer,
+rem when the default settings in the .exe are inappropriate.
+rem
+rem Preference is given to any java VM embedded with JOverseer.
+rem
+rem To force the use of your own version of java over the embedded version,
+rem just create the file DontUseCustomJava.txt in the JO directory,  
+rem or add DontUseCustomJava as the first argument.
+rem
+rem To force disable the 2d/3d options causing problems for some NVidia drivers,
+rem just create the file DontUse3d.txt in the JO directory,
+rem or add DontUse3d as the first or second argument.
+rem
 SETLOCAL ENABLEDELAYEDEXPANSION
 if not exist joverseer.jar (
-  echo Current directory is %CD%
+  echo Current directory is "%CD%"
   echo "This batch file needs to be run from directory C:\Program Files (x86)\JOverseer\"
   pause
   goto end
@@ -12,8 +25,13 @@ if exist "DontUseCustomJava.txt" (
   echo Ignoring custom version of java in JOverseer
   goto notlocal
 )
+if "%1"=="DontUseCustomJava" (
+  echo Ignoring custom version of java in JOverseer
+  shift
+  goto notlocal
+)
 SET count=1
-set %var1%=
+set var1=
 FOR /F "tokens=*" %%F in ('jre\bin\java -version 2^>^&1') DO (
   SET var!count!=%%F
   SET /a count=!count!+1
@@ -56,7 +74,18 @@ if !M! leq 8 (
     echo If running the version of java embedded with JOverseer doesn't work for you, consider installing
     echo https://adoptium.net/en-GB/temurin/releases/?version=11 
 )
-%JOJAVA% -Xmx512M  -Djava.net.preferIPv4Stack=true %EXPORTS% -jar joverseer.jar %1 %2 %3 %4 %5 %6 %7
+set NO3D=
+if exist "DontUse3d.txt" (
+  echo Disabling 2d/3d options
+  set NO3D=-Dsun.java2d.ddoffscreen=false -Dsun.java2d.d3d=false -Dsun.java2d.noddraw=true
+)
+if "%1"=="DontUse3d" (
+  echo Disabling 2d/3d options
+  set NO3D=-Dsun.java2d.ddoffscreen=false -Dsun.java2d.d3d=false -Dsun.java2d.noddraw=true
+  shift
+)
+echo %JOJAVA% -Xmx512M  -Djava.net.preferIPv4Stack=true %EXPORTS% %NO3D% -jar joverseer.jar %1 %2 %3 %4 %5 %6 %7
+%JOJAVA% -Xmx512M  -Djava.net.preferIPv4Stack=true %EXPORTS% %NO3D% -jar joverseer.jar %1 %2 %3 %4 %5 %6 %7
 if not %ERRORLEVEL% equ 0 (
   echo got error %ERRORLEVEL%
   pause

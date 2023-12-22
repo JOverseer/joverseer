ee56737f7ddd7971d930fae93f9d645038796ad0
Sam-Terrett <samuelterrett1@gmail.com>
2023-12-22 14:52:30 +0000

    Menu opens panel in correct place

--- joverseerjar/src/org/joverseer/ui/views/DiploMessage.java
+++ joverseerjar/src/org/joverseer/ui/views/DiploMessage.java
@@ -3,9 +3,39 @@
  */
 package org.joverseer.ui.views;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.joverseer.ui.support.Messages;
+import org.springframework.richclient.application.support.AbstractView;
+import org.springframework.richclient.layout.TableLayoutBuilder;
+import org.joverseer.ui.BaseView;
+
 /**
  * 
  */
-public class DiploMessage extends BaseHtmlReportView {
+public class DiploMessage extends BaseView {
+	JTextField DiplomaticMess;
+	JPanel panel;
+	
+	@Override
+	protected JComponent createControl() {
+		this.DiplomaticMess = new JTextField();
+		this.panel = new JPanel();
+		this.panel.add(this.DiplomaticMess);
+//		JPanel p = new JPanel();
+//		p.add(DiplomaticMess);
+//		
+//		TableLayoutBuilder tlb = new TableLayoutBuilder();
+//		tlb.cell(new JLabel("Here")); //$NON-NLS-1$
+//		tlb.gapCol();
+
+
+		return this.panel;
+		
+	}
 
 }

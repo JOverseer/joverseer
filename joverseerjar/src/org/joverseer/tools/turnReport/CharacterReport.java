805c8a4742072c9ff9b5daf61e9dfafc51ad3274
Sam-Terrett <samuelterrett1@gmail.com>
2024-04-06 15:18:22 +0100

    Double Agents in Turn Repost
    Creates a new table for displaying double agents, and they no longer appear in "other characters"
    - They are colour coded, green for new double agent, yellow for unchanged, and red if a character is no longer a double agent
    - "notes" column now says which nation the character is doubled for.

--- joverseerjar/src/org/joverseer/tools/turnReport/CharacterReport.java
+++ joverseerjar/src/org/joverseer/tools/turnReport/CharacterReport.java
@@ -74,6 +74,11 @@ public class CharacterReport extends BaseReportObject implements IBelongsToNatio
 	@Override
 	public String getLinks() {
 		String ret = super.getLinks() + (getCharacter() != null && getCharacter().getOrderResults() != null ? " <a href='http://event?report=" + getCharacter().getId().replace(" ", "_") + "'>Report</a>" : "");
+		if(getCharacterWrapper() != null) {
+			if(getCharacterWrapper().isDoubleAgent() == true && getCharacterWrapper().getOrderResults() != null) {
+				ret = super.getLinks() + (" <a href='http://event?report=" + getCharacterWrapper().getId().replace(" ", "_") + "'>Report</a>");
+			}
+		}
 		for (Encounter enc : this.turn.getEncounters(getName())) {
 			ret += " <a href='http://event?enc=" + enc.getHexNo() + "," + Character.getIdFromName(enc.getCharacter()).replace(" ", "_") + "'>Enc</a>";
 		}
